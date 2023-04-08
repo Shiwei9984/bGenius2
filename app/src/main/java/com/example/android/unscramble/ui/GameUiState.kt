93eb05510e35e2d77d678a94f10ce9757e9a1fd6
Shiwei9984 <126206359+Shiwei9984@users.noreply.github.com>
2023-04-08 14:23:35 -0400

    Delete app directory

--- app/src/main/java/com/example/android/unscramble/ui/GameUiState.kt
+++
@@ -1,7 +0,0 @@
-data class GameUiState(
-    val currentScrambleWord: String = "",
-    val isGuessedWordWrong : Boolean = false,
-    val score : Int = 1,
-    val currentWordCount: Int = 0,
-    val isGameOver: Boolean = false
-)
